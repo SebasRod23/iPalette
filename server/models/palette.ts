6228f7ebd18108dbadc8a9f27ff52f60ad841db9
SebasRod23 <sebastian_rodriguezg@yahoo.com>
2021-10-26 23:24:03 -0500

    Create palette model

---
+++ server/models/palette.ts
@@ -0,0 +1,29 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+export interface iPalette extends Document {
+  userId: string;
+  name: string;
+  colors: [string];
+}
+
+const paletteSchema = new Schema<iPalette>(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    colors: {
+      type: [String],
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const Palette = mongoose.model('Palette', paletteSchema);
+
+export default Palette;
