4add98a19623a432d4de895de5871c098332b568
SebasRod23 <sebastian_rodriguezg@yahoo.com>
2021-10-29 15:19:15 -0500

    Add auth form component and router

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-const App: React.FC = () => {
-  return (
-    <div>
-      <h1>iPalette v2</h1>
-    </div>
-  );
-};
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 );
